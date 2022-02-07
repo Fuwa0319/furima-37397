cc3a9a9fc6953f5298c07a070e4f380b0e7be766
Satoshi0118 <fwstr0319@gmail.com>
2022-02-07 11:54:04 +0900

    itemモデル修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -82,7 +82,6 @@ RSpec.describe Item, type: :model do
       it 'priceが全角数値を含んでいると登録できない' do
         @item.price = '５００'
         @item.valid?
-        binding.pry
         expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
     end

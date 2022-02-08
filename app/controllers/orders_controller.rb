f13b7299b611519f7d6f34cf684a5b4d77e2b18e
Satoshi0118 <fwstr0319@gmail.com>
2022-02-08 23:44:04 +0900

    商品購入機能実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,40 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
+
+  def index
+    @item = Item.find(params[:item_id])
+    if @item.order.nil? && current_user.id != @item.user_id
+      @order_address = OrderAddress.new
+    else
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render 'index'
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :cell_hone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+end
